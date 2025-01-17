44eca96bd13bb9311b403a3004c7c1779a5ddf90
CallousJester <robert.chamberland007@gmail.com>
2025-01-16 20:35:29 -0800

    C: sorts char by occurence # and prints report

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)     #total number of words
     character_count = get_char_count(text)  # total number of alphabet characters only
-    print(f"--- Begin report of {book_path} --- \n {num_words} words were found in the document \n \n {character_count} \n --- End report ---")
+    sorted_list = sort_count(character_count)
+
+    print(f"--- Begin report of {book_path} --- ") #Prints report title
+    print(f"{num_words} words were found in the document\n") #prints number of words
+    for char_dict in sorted_list:
+        char = char_dict["char"]
+        count = char_dict["num"]
+        print(f"The '{char}' charcter was found {count} times")
+    print("--- End report ---")
+
 
 def get_char_count(text):
     char_dict = {}
@@ -15,21 +24,21 @@ def get_char_count(text):
                 char_dict[character] = 1
             else:
                 char_dict[character] += 1   # add to counter
-        return char_dict
+        
 
-#goal: have characters sorted by number of appearances + print a report showing this.
-# - sort the characters in the "char_key" by #
-# - add wording to the print
-    # - make it print the key entry
-    # - make it print the key value
-    # - have text between the two
-        ## this may be a for loop in the print somehow? that way it cycles through each character
+    # Convert dictionary to list of dictionaries
+    char_list = []
+    for char, num in char_dict.items():
+        char_list.append({"char": char, "num": num})
+    return char_list
 
-def sort_count(char_dict):
-    #make list only include letters
+def sort_on(char_list):
+    return char_list["num"] # returns the value of the "num" in the dictionary
 
-    char_dict.sort(reverse=True, key=sort_count)
-    return char_dict
+def sort_count(char_list):
+
+    char_list.sort(reverse=True, key=sort_on) # sorts the characters
+    return char_list
 
 def get_num_words(text):
     words = text.split()
